7c17a4c6ba508d39b8d415244f24f5e65a118437
KniokWu <kniokwu@gmail.com>
2023-12-12 16:19:04 +0800

    1. Updated 0-1背包问题 滚动背包版本.

--- backpack-01-compressed/solution.go
+++ backpack-01-compressed/solution.go
@@ -10,6 +10,47 @@ package main
 
 import "fmt"
 
+//func main() {
+//	var n, capacity int
+//	fmt.Scan(&n, &capacity)
+//
+//	values := make([]int, n)
+//	weights := make([]int, n)
+//
+//	for i := 0; i < n; i++ {
+//		fmt.Scan(&weights[i], &values[i])
+//	}
+//
+//	dp := make([]int, capacity+1)
+//
+//	for i := weights[0]; i <= capacity; i++ {
+//		dp[i] = values[0]
+//	}
+//
+//	for i := 1; i < n; i++ {
+//		newDp := make([]int, capacity+1)
+//		for j := 0; j <= capacity; j++ {
+//			// 1. 如果无法选择当前物件 直接继承上一物件
+//			if j < weights[i] {
+//				newDp[j] = dp[j]
+//			}
+//
+//			// 2. 可以选择当前物件
+//			if j >= weights[i] {
+//				curValue := values[i] + dp[j-weights[i]]
+//				if curValue > dp[j] {
+//					newDp[j] = curValue
+//				} else {
+//					newDp[j] = dp[j]
+//				}
+//			}
+//		}
+//		dp = newDp
+//	}
+//
+//	fmt.Println(dp[capacity])
+//}
+
 func main() {
 	var n, capacity int
 	fmt.Scan(&n, &capacity)
@@ -28,24 +69,18 @@ func main() {
 	}
 
 	for i := 1; i < n; i++ {
-		newDp := make([]int, capacity+1)
-		for j := 0; j <= capacity; j++ {
-			// 1. 如果无法选择当前物件 直接继承上一物件
+		for j := capacity; j >= 0; j-- {
 			if j < weights[i] {
-				newDp[j] = dp[j]
+				break
 			}
 
-			// 2. 可以选择当前物件
 			if j >= weights[i] {
 				curValue := values[i] + dp[j-weights[i]]
 				if curValue > dp[j] {
-					newDp[j] = curValue
-				} else {
-					newDp[j] = dp[j]
+					dp[j] = curValue
 				}
 			}
 		}
-		dp = newDp
 	}
 
 	fmt.Println(dp[capacity])
